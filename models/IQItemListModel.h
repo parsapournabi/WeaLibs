c8bf9a31eb479780ea0f7867ee2420fb7362b087
parsa <hkh@arvand.com>
2025-06-10 13:09:22 +0330

    QItemListModelBase Renamed to Interface
    renamed: IQItemListModel.h

--- models/IQItemListModel.h
+++ models/IQItemListModel.h
@@ -1,5 +1,5 @@
-#ifndef QITEMLISTMODDELBASE_H
-#define QITEMLISTMODDELBASE_H
+#ifndef IQITEMLISTMODDELBASE_H
+#define IQITEMLISTMODDELBASE_H
 
 #include <QObject>
 #include <QAbstractListModel>
@@ -12,22 +12,23 @@
 #include <QVariant>
 
 
-class QItemListModelBase : public QAbstractListModel {
+class IQItemListModel : public QAbstractListModel {
     Q_OBJECT
 public:
-    explicit QItemListModelBase(QObject *parent = nullptr) :
-        QAbstractListModel(parent) {}
+    explicit IQItemListModel(QObject *parent = nullptr) : QAbstractListModel(parent) {}
 
     virtual Q_INVOKABLE int insertItem(QObject* itemObj, quint32 itemId) = 0;
 
     virtual Q_INVOKABLE bool removeItem(quint32 itemId) = 0;
+
     virtual Q_INVOKABLE bool editItem(int key, QObject* itemObj) = 0;
 
     virtual Q_INVOKABLE QVariantList getRowData(int row) const = 0;
+
     virtual Q_INVOKABLE void selectItem(int index) = 0;
 
     virtual Q_INVOKABLE QVariantList getHeadersData(bool getParent = true) const = 0;
 
 };
 
-#endif // QITEMLISTMODDELBASE_H
+#endif // IQITEMLISTMODDELBASE_H
